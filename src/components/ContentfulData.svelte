5b51041f47964e44e9f74f61c1fb263fb2aa0be3
Nawanit <nawanit.s@ignitiv.com>
2022-01-31 12:53:32 +0530

    Seperated components

---
+++ src/components/ContentfulData.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let contentfulData;
+  onMount(async () => {
+    const client = contentful.createClient({
+      space: '9jo7ly0kaj2i',
+      accessToken: '6cjsFFuJF9j3g1xD1jy8FhDEzvy8v_oL9oSSFrlJHyo'
+    });
+    console.log(client);
+
+    client
+      .getEntry('43s8eOA2aUGusOmLX5229J')
+      .then(res => {
+        contentfulData = res.fields.bannerHeader;
+      })
+      .catch(console.log);
+  });
+</script>
+
+<div class="ContentfulData">
+  <p>{contentfulData}</p>
+</div>
